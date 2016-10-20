9b87c9a53130777fcf58c3e4f8f3a91838a46312
German Blanco <russo@batman>
2016-10-20 16:01:10 -0300

    prueba con jaxas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,9 @@
         <title>Laravel</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        
+<script src="http://code.jquery.com/jquery-3.1.1.min.js"   integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="   crossorigin="anonymous"></script>
+        
 
         <style>
             html, body {
@@ -40,6 +43,48 @@
             <div class="content">
                 <div class="title">Laravel 5</div>
             </div>
+
+            <a id="open" href="#">ver</a>
         </div>
     </body>
+
+<script>
+            
+
+            /*$('#open').click(function() {
+                var id = 5;
+                var ajaxurl = {{ URL::to('computer/id') }};
+                ajaxurl = ajaxurl.replace(':id', id);
+                $.ajax({
+                    url: ajaxurl,
+                    type: "GET",
+                    success: function(data){
+                        $data = $(data); // the HTML content that controller has produced
+                        $('#item-container').hide().html($data).fadeIn();
+                    }
+                });
+
+                conslole.log('hola');
+            });*/
+
+            $('body').on('click', '#open', function(){
+                console.log('hola');
+                var id = 5;
+                
+                var ajaxurl = 'computer/' + id;
+                ajaxurl = ajaxurl.replace(':id', id);
+                $.ajax({
+                    url: ajaxurl,
+                    type: "GET",
+                    success: function(data){
+                        $data = $(data); // the HTML content that controller has produced
+                        console.log(data);
+                    }
+                });
+            });
+
+        </script>
+
+
+
 </html>
